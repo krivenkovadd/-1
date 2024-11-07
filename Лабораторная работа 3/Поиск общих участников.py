3fd66ed0df7249c2d7a2d2bb0e375db0b6bfabcc
krivenkovadd <krivenkovadd@gmail.com>
2024-11-08 00:38:21 +0300

    Update Поиск общих участников.py

--- Лабораторная работа 3/Поиск общих участников.py
+++ Лабораторная работа 3/Поиск общих участников.py
@@ -1,10 +1,10 @@
-def find_common_participants(participants_first_group, participants_second_group, sep = ','):
-    participants_first = set(participants_first_group.split(sep))
-    participants_second = set(participants_second_group.split(sep))
+def find_common_participants(first_group, second_group, sep = ','):
+    participants_first = set(first_group.split(sep))
+    participants_second = set(second_group.split(sep))
     common_participants = participants_first.intersection(participants_second)
     return list(common_participants)
 
-participants_first_group = "Иванов|Петров|Сидоров"
-participants_second_group = "Петров|Сидоров|Смирнов"
+first_group = "Иванов|Петров|Сидоров"
+second_group = "Петров|Сидоров|Смирнов"
 
-print(find_common_participants(participants_first_group,participants_second_group, sep = '|'))
+print(find_common_participants(first_group, second_group, sep = '|'))
